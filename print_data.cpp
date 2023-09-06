7fd796dbc87fb97376b2514d9eaae3e9125fbba9
sasha <sashok>
2023-09-06 22:36:23 +0300

    01.09.2023

---
+++ print_data.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void PrintDataTriagle ( const char *str, size_t rows, size_t cols );
+void PrintDataLine ( const char *str, size_t rows, size_t cols );    //
+
+void PrintDataTriagle ( const char *str, size_t rows, size_t cols )
+{
+    int n = 0;
+    for ( int j = 1; j <= rows; ++j ) {
+        if ( j == rows ) {
+            for ( int i = 0; i < cols; ++i ) {
+                printf ( "%d", *(str + n + i ) );
+            }
+        }
+
+        else {
+            for ( int i = 0; i < j; ++i ) {
+                printf ( "%d", *(str + n + i ) );
+            }
+        }
+        n = n + j;
+        printf ( "\n" );
+    }
+
+}
+
+void PrintDataLine ( const char *str, size_t rows, size_t cols )
+{
+    for ( int i = 0; i < rows; ++i ) {
+        for ( int j = 0; j < cols; ++j ) {
+            printf ( "%c", *(str + cols * i + j ) );
+        }
+        printf ( "\n" );
+    }
+}

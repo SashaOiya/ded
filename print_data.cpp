3337afd10ab49e86390eef0e387ba7e0bd2bf4d4
sasha <sashok>
2023-09-07 02:05:32 +0300

    Plus arrays of ptr

--- print_data.cpp
+++ print_data.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 void PrintDataTriagle ( const char *str, size_t rows, size_t cols );
-void PrintDataLine ( const char *str, size_t rows, size_t cols );    //
+void PrintDataLine ( const char *str, size_t rows, size_t cols );
+void PrintDataPtrArray ( const char *str, size_t rows, size_t cols );
 
-void PrintDataTriagle ( const char *str, size_t rows, size_t cols )
+void PrintDataTriagle ( char *str, size_t rows, size_t cols )
 {
     int n = 0;
     for ( int j = 1; j <= rows; ++j ) {
@@ -30,6 +31,16 @@ void PrintDataLine ( const char *str, size_t rows, size_t cols )
         for ( int j = 0; j < cols; ++j ) {
             printf ( "%c", *(str + cols * i + j ) );
         }
+        printf ( "\n" );
+    }
+}
+
+void PrintDataPtrArray ( char *str[], size_t rows, size_t cols )
+{
+    for ( int i = 0; i < rows; ++i ) {
+        //for ( int j = 0; j < cols; ++j ) {
+            printf ( "%s", str[i] );
+        //}
         printf ( "\n" );
     }
 }

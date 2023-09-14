d17080ef69aa9186c9ddc13a3bfb6cb3c70d11ef
sasha <sashok>
2023-09-14 10:58:34 +0300

    Task 5

--- file.cpp
+++ file.cpp
@@ -1,31 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #include "my_strdup.cpp"
 #include "print_data.cpp"
+#include "getfilesize.cpp"
 
 int main()
 {
     FILE *f = fopen ( "text.txt", "r" );
 
-    const int line = 100;
-    const int nlines = 4;
-    char * *text4 = (char **)calloc ( nlines, sizeof ( char * ) );
+    int nlines = 0;
+    int value = GetFileSize ( f );
 
-    for ( int p = 0; p < nlines; ++p ) {
-        char buffer[100] = {};
-        text4[p] = strdup ( fgets ( buffer, sizeof ( buffer ), f ) );
+    char *buffer = (char *)calloc ( value + 1, sizeof ( char ) );
+    fread ( buffer, sizeof( char ), value, f );//error
+    buffer[value] = '\n';
+
+    for ( int i = 0; i <= value; ++i ) {
+        if ( buffer[i] == '\n' ) {
+            ++nlines;
+            buffer[i] = '\0';
+        }
     }
 
-    PrintDataPtrArray ( text4, nlines, line );
+    char * *text5 = (char **)calloc ( nlines, sizeof ( char * ) );
+
+    text5[0] = buffer;
+
+    for ( int i = 1, j = 1; i < value  && j < nlines; ++i ) {
+        if ( buffer[i] == '\0' ) {
+            text5[j++] = my_strdup ( buffer + i + 1 );
+        }
+    }
+
+    PrintDataPtrArray ( text5, nlines );
 
     for ( int i = 0; i < nlines; ++i ) {
-        free ( text4[i] ) ;
-        text4[i] = nullptr;
+        free ( text5[i] );
+        text5[i] = nullptr;
     }
 
     fclose ( f );
 
     return 0;
 }
+

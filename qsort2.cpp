89bed3d40423a03bf6cce01faf8ec4a9ac89a297
sasha <sashok>
2023-09-14 22:13:40 +0300

    vers.2

--- qsort2.cpp
+++ qsort2.cpp
@@ -2,12 +2,40 @@
 #include <stdlib.h>
 #include "print_data.cpp"
 
-int sort ( char *data, int left, int right )
+int portition ( char *data, int left, int right );
+void qsort ( char *data, int left, int right );
+
+enum Value_t {
+    RIGHT_POINTER = 1,
+    LEFT_POINTER   = 2
+};
+
+int main ()
 {
-    const int mid = ( right - left ) / 2;
+    char data[] = { 2, 7, 5, 6, 9, 8, 1 };
+    qsort ( data, 0,  sizeof ( data ) / sizeof ( char ) - 1 );  //6   // sizeof ( data ) / sizeof ( char )
+    //printf ( "%d\n", mid );
+    //printf ( "%s\n", data );
+    PrintDataLine ( data, 1,  sizeof ( data ) / sizeof ( char )  );
+
+    return 0;
+}
+
+int portition ( char *data, int left, int right )
+{
+    int mid = 0;
+    mid = ( right - left ) / 2;
+    /*if (      ( right - left ) % 2 == 0 ) {
+        mid = ( right - left ) / 2;
+    }
+    else if ( ( right - left ) % 2 == 1 ) {
+        mid = ( right - left ) / 2;
+    } */
+
     char value = data[mid];
+    //printf ( "%d\n", value );
     int pointer = 0;
-    char *buffer =( char *)calloc( right, sizeof ( char ) );
+    //char *buffer =( char *)calloc( right, sizeof ( char ) );
     int j = right;
     int g = 0;
     int flag = 0;
@@ -15,46 +43,76 @@ int sort ( char *data, int left, int right )
     //char *buffer = (char *)calloc ( value + 1, sizeof ( char ) );
     printf ( "meow\n" );
 
-    for ( int i = 0; left + i < mid && j >= mid; ++i, --j ) {
+    for ( int i = 0; left + i <= mid && j >= mid; ) {
         if ( data[i] >= value && flag == 0 ) {
             pointer = i;
-            flag = 2;
-            bla = data[i];
+            flag    = LEFT_POINTER;
+            bla     = data[i];
         }
-        else if ( data[i] >= value && flag == 1 ) {
+        else if ( data[i] >= value && flag == RIGHT_POINTER ) {
+            data[pointer] = data[i];
+            data[i]       = bla;
+            flag          = 0;
+        }
+        /*else if ( data[i] >= value && flag == LEFT_POINTER && i == mid ) {
+            data[pointer] = data[i];
+            data[i]       = bla;
+            flag          = 0;
+        } */
+        /*printf ( "%d\t%d\t", j, i );
+        printf ( "%d\n", bla ); */
+        if ( data[j] < value && flag == LEFT_POINTER && j != mid ) {     //enum
             data[pointer] = data[j];
             data[j] = bla;
             flag = 0;
         }
-        if ( data[j] < value && flag == 2 ) {     //enum
-            data[pointer] = data[j];
-            data[j] = bla;
-            flag = 0;
-        }
-        else if ( data[j] < value && flag == 0 ) {
+        else if ( data[j] < value && flag == 0 ) {   // 0 enum
             pointer = j;
-            flag = 1;
+            flag = RIGHT_POINTER;
             bla = data[j];
         }
+
+        else if ( data[j] <= value && flag == LEFT_POINTER && j == mid ) {
+            data[pointer] = data[j];
+            data[j]       = bla;
+            flag          = 0;
+        }
+        if ( flag == RIGHT_POINTER ) {
+            ++i;
+        }
+        else if ( flag == LEFT_POINTER ) {
+            --j;
+        }
+        else {
+            ++i;
+            --j;
+        }
     }
-    for ( int i = 0; right - i >= mid && g >= 0; ++i ) {
+    /*for ( int i = 0; right - i >= mid && g >= 0; ++i ) {
         if ( data[i] > value && g - 1 > 0 ) {
             data[i] = buffer[g--] ;
         }
-    }
+    } */
 
     //free
 
     return mid;
 }
 
-int main ()
+void qsort ( char *data, int left, int right )
 {
-    char data[] = { 9, 8, 8, 4, 6, 7 };
-    int mid = sort ( data, 0, 5 );  //6
-    //printf ( "%d\n", mid );
-    //printf ( "%s\n", data );
-    PrintDataLine ( data, 1, 6 );
+    static int flag = 0;
+    int mid = portition ( data, left, right );
+    if ( mid != 1 && flag == 0 ) {
+        qsort ( data, left, mid - 1 );
+        flag = 1;
+    }
+    else if ( mid != 1 && flag == 1 ) {
+        qsort ( data, left, mid - 1 );
+        flag = 2;
+    }
+    /*if ( right - mid != 1 && flag == 1 ) {
+        qsort ( data, mid, right );
+    } */
 
-    return 0;
 }
